82cef1ca3b251825e16563464b64b2bf7de54ff4
He, Jiehui <he_jiehui@163.com>
2015-10-08 17:28:54 +0800

    revised

--- com.xross.tools.xeda.editor/src/com/xross/tools/xeda/editor/figures/XedaDiagramFigure.java
+++ com.xross.tools.xeda.editor/src/com/xross/tools/xeda/editor/figures/XedaDiagramFigure.java
@@ -1,5 +1,13 @@
 package com.xross.tools.xeda.editor.figures;
 
+import org.eclipse.draw2d.Figure;
+import org.eclipse.draw2d.XYLayout;
 
-public class XedaDiagramFigure{
+import com.xross.tools.xeda.editor.model.XedaConstants;
+
+
+public class XedaDiagramFigure extends Figure implements XedaConstants{
+    public XedaDiagramFigure() {
+    	setLayoutManager(new XYLayout());
+    }
 }
