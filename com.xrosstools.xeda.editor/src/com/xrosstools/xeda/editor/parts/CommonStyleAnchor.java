ca45c64e6c70fa19268435f6332f2ce5dddaa666
hejiehui <he_jiehui@163.com>
2017-08-13 12:41:49 +0800

    add common router

---
+++ com.xrosstools.xeda.editor/src/com/xrosstools/xeda/editor/parts/CommonStyleAnchor.java
@@ -0,0 +1,101 @@
+package com.xrosstools.xeda.editor.parts;
+
+import org.eclipse.draw2d.ChopboxAnchor;
+import org.eclipse.draw2d.IFigure;
+import org.eclipse.draw2d.geometry.Point;
+import org.eclipse.draw2d.geometry.Rectangle;
+
+import com.xrosstools.xeda.editor.model.RouteStyle;
+
+public class CommonStyleAnchor extends ChopboxAnchor {
+    private RouteStyle style;
+    private boolean isSource;
+    public CommonStyleAnchor(IFigure owner, RouteStyle style, boolean isSource) {
+        super(owner);
+        this.style = style;
+        this.isSource = isSource;
+    }
+    public Point getLocation(Point loc) {
+    	return style == RouteStyle.direct ? 
+    			super.getLocation(loc) :
+    				isSource ? 
+    						getSourceLocation(loc) : 
+    							getTargetLocation(loc);
+    }
+
+    private Point getSourceLocation(Point ref)
+    {
+        Rectangle r = new Rectangle(getBox());
+        
+        Point pos;
+        getOwner().translateToAbsolute(r);
+        
+        if(style == RouteStyle.heightFirst) {
+            if(ref.y < r.y())
+                pos = r.getTop();
+            else if(ref.y > r.y + r.height)
+                pos = r.getBottom();
+            else if(ref.x < r.x) {
+            	if(ref.y >= r.y && ref.y <= r.y + r.height)
+            		pos = new Point(r.x, ref.y);
+            	else
+            		pos = r.getLeft();
+            }else {
+            	if(ref.y >= r.y && ref.y <= r.y + r.height)
+            		pos = new Point(r.x + r.width, ref.y);
+            	else
+            		pos = r.getRight();
+            }
+        }else{
+            if(ref.x < r.x)
+                pos = r.getLeft();
+            else  if(ref.x > r.x + r.width)
+                pos = r.getRight();
+            else if(ref.y < r.y) {
+            	if(ref.x >= r.x && ref.x <= r.x + r.width)
+            		pos = new Point(ref.x, r.y);
+            	else
+            		pos = r.getTop();
+        	}else {
+            	if(ref.x >= r.x && ref.x <= r.x + r.width)
+            		pos = new Point(ref.x, r.y + r.height);
+            	else
+            		pos = r.getBottom();
+        	}
+        }
+            
+        Point p = new Point(pos);
+        return p;
+    }
+
+    private Point getTargetLocation(Point ref)
+    {
+    	Rectangle r = new Rectangle(getBox());
+        
+        Point pos;
+        getOwner().translateToAbsolute(r);
+        
+        if(style == RouteStyle.heightFirst) {
+            if(ref.x < r.x)
+                pos = r.getLeft();
+            else if(ref.x > r.x + r.width)
+                pos = r.getRight();
+            else if(ref.y < r.y)
+                pos = new Point(ref.x, r.y);
+            else
+                pos = new Point(ref.x, r.y + r.height);
+        }else{
+            if(ref.y < r.y)
+                pos = r.getTop();
+            else if(ref.y > r.y + r.height)
+                pos = r.getBottom();
+            else if(ref.x < r.x)
+            	pos = new Point(r.x, ref.y);
+            else
+            	pos = new Point(r.x + r.width, ref.y);
+        }
+            
+        Point p = new Point(pos);
+        return p;
+    }
+}
---
+++ com.xrosstools.xeda.editor/src/com/xrosstools/xeda/editor/parts/CommonStyleRouter.java
@@ -0,0 +1,52 @@
+package com.xrosstools.xeda.editor.parts;
+
+import org.eclipse.draw2d.AbstractRouter;
+import org.eclipse.draw2d.Connection;
+import org.eclipse.draw2d.geometry.Point;
+import org.eclipse.draw2d.geometry.PointList;
+
+import com.xrosstools.xeda.editor.model.RouteStyle;
+
+public class CommonStyleRouter extends AbstractRouter {
+    private RouteStyle style;
+
+    public CommonStyleRouter(RouteStyle style) {
+        this.style = style;
+    }
+    
+    public void setStyle(RouteStyle style) {
+        this.style = style;
+    }
+
+    @Override
+    public void route(Connection conn) {
+        PointList pl = conn.getPoints();
+        pl.removeAllPoints();
+        Point start = getStartPoint(conn);
+        conn.translateToRelative(start);
+        Point end = getEndPoint(conn);
+        conn.translateToRelative(end);
+
+        if (style == RouteStyle.direct) {
+            pl.addPoint(start);
+            pl.addPoint(end);
+            return;
+        }
+
+        Point middle;
+        pl.addPoint(start);
+        if (style == RouteStyle.heightFirst) {
+            middle = new Point(start.x, end.y);
+            pl.addPoint(middle);
+            if(middle.x == end.x)
+            	return;
+        } else {
+            middle = new Point(end.x, start.y);
+            pl.addPoint(middle);
+            if(middle.y == end.y)
+            	return;
+        }
+        
+        pl.addPoint(end);
+    }
+}
